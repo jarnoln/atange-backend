9e1c96baf8a6cf276f0364639f08ed02cfcade06
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2022-03-07 18:22:06 +0200

    Add base template

--- collective/models.py
+++ collective/models.py
@@ -6,3 +6,6 @@ class Collective(models.Model):
     title = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     edited = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return '{}:{}'.format(self.name, self.title)
