e51d0ae815fe08ace95c8a4e90ab2c558b6964a3
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2022-04-21 07:40:45 +0300

    Use SlugField for Collective name

--- collective/models.py
+++ collective/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Collective(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.SlugField(max_length=100)
     title = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     edited = models.DateTimeField(auto_now=True, null=True, blank=True)
