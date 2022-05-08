603d2a849fd39a851a2fa0f93ed87d34f34530d4
Jarno Luoma-Nirva <jarno.luoma-nirva@vaisto.io>
2022-05-08 15:19:22 +0300

    Set up basic logging

--- atange/settings.py
+++ atange/settings.py
@@ -136,6 +136,20 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
