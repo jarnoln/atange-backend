7d07faba4c7faf9c8a3ad3146ab8e60f611f52dd
Jarno Luoma-Nirva <jln@iki.fi>
2022-05-21 12:00:25 +0800

    Add QuestionnaireItem model

--- collective/models.py
+++ collective/models.py
@@ -13,3 +13,26 @@ class Collective(models.Model):
 
     def __str__(self):
         return '{}:{}'.format(self.name, self.title)
+
+
+class QuestionnaireItem(models.Model):
+    ITEM_TYPES = [
+        ('Q', 'Question'),
+        ('H', 'Header')
+    ]
+    collective = models.ForeignKey(Collective, on_delete=models.CASCADE)
+    name = models.SlugField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True, default='')
+    order = models.IntegerField(
+        default=0, blank=True,
+        help_text='Determines the order between items at same level.'
+    )
+    item_type = models.CharField(max_length=1, default='Q', choices=ITEM_TYPES)
+    parent = models.ForeignKey('self', null=True, blank=True, default=None, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    creator = models.ForeignKey(User, null=True, blank=True, default=None, on_delete=models.CASCADE)
+    edited = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return '{}:{}:{}:{}'.format(self.collective.name, self.item_type, self.name, self.title)
--- collective/tests/test_models.py
+++ collective/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from collective.models import Collective
+from collective.models import Collective, QuestionnaireItem
 
 
 class CollectiveModelTests(TestCase):
@@ -15,3 +15,29 @@ class CollectiveModelTests(TestCase):
     def test_string(self):
         collective = Collective.objects.create(name='jla', title="JLA")
         self.assertEqual(str(collective), 'jla:JLA')
+
+
+class QuestionnaireItemModelTests(TestCase):
+    def test_can_save_and_load(self):
+        creator = User.objects.create_user(username='superman', password='Man_of_Steel')
+        collective = Collective.objects.create(name='jla', title='JLA', creator=creator)
+        item = QuestionnaireItem(collective=collective, name='Q1', title='Question 1')
+        item.save()
+        self.assertEqual(QuestionnaireItem.objects.count(), 1)
+        self.assertEqual(QuestionnaireItem.objects.first(), item)
+
+    def test_string(self):
+        creator = User.objects.create_user(username='superman', password='Man_of_Steel')
+        collective = Collective.objects.create(name='jla', title='JLA', creator=creator)
+        item = QuestionnaireItem.objects.create(collective=collective, name='Q1', title='Question 1')
+        self.assertEqual(str(item), 'jla:Q:Q1:Question 1')
+
+    def test_default_values(self):
+        creator = User.objects.create_user(username='superman', password='Man_of_Steel')
+        collective = Collective.objects.create(name='jla', title='JLA', creator=creator)
+        item = QuestionnaireItem.objects.create(collective=collective, name='Q1', title='Question 1')
+        self.assertEqual(item.description, '')
+        self.assertEqual(item.order, 0)
+        self.assertEqual(item.creator, None)
+        self.assertEqual(item.item_type, 'Q')
+        self.assertEqual(item.parent, None)
