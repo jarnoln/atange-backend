7e0b4b6d63b37ecdc050cee8b2e48e8c310d3c2e
Jarno Luoma-Nirva <jln@iki.fi>
2022-06-03 13:39:11 +0800

    Back to using default SQLite file location

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ pip install pip --upgrade
 pip install -r requirements.txt
 pip install gunicorn
 
-ls /var/data
-touch /var/data/db.sqlite3
-
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
