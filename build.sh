de1c7a730e256fdbc0052be91000ecbbaee8e994
Jarno Luoma-Nirva <jln@iki.fi>
2022-06-03 13:27:35 +0800

    Do not migrate during deploy

--- build.sh
+++ build.sh
@@ -6,10 +6,6 @@ pip install pip --upgrade
 pip install -r requirements.txt
 pip install gunicorn
 
-sleep 10
-ls /var/data
-touch /var/data/db.sqlite3
-
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
