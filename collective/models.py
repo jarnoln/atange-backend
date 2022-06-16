c61e00de5e04fa9efeeb9ea26b78edb06fa3e15e
Jarno Luoma-Nirva <jln@iki.fi>
2022-06-16 13:58:44 +0800

    Can edit if member of admin group

--- collective/models.py
+++ collective/models.py
@@ -62,7 +62,16 @@ class Collective(models.Model):
     def get_permissions(self, user):
         logger = logging.getLogger(__name__)
         logger.debug("Collective:get_permissions user:{}".format(user))
-        permissions = {"can_edit": user == self.creator, "can_join": False}
+        can_edit = False
+        if user == self.creator:
+            can_edit = True
+        elif self.admin_group:
+            can_edit = self.admin_group.is_member(user)
+
+        permissions = {
+            "can_edit": can_edit,
+            "can_join": False
+        }
         logger.debug("  permissions:{}".format(permissions))
         return permissions
 
--- collective/tests/test_api_collective_permissions.py
+++ collective/tests/test_api_collective_permissions.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from .auth_test_case import AuthTestCase
-from collective.models import Collective
+from collective.models import Collective, UserGroup
 
 
 class CollectivePermissionsTests(AuthTestCase):
@@ -45,16 +45,20 @@ class CollectivePermissionsTests(AuthTestCase):
         self.assertEqual(data_out["can_edit"], False)
         self.assertEqual(data_out["can_join"], False)
 
-    def test_get_collective_permissions_when_not_creator(self):
+    def test_get_collective_permissions_when_not_creator_but_admin(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
         creator = User.objects.create(username="batman", password="ImBatman")
         collective = Collective.objects.create(
             name="jla", title="JLA", description="", creator=creator
         )
+        admin_group = UserGroup.objects.create(name='jla_admins', title='JLA administrators')
+        admin_group.add_member(self.user)
+        collective.admin_group = admin_group
+        collective.save()
         response = self.client.get(reverse("collective_permissions", args=[collective.name]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data_out = json.loads(response.content.decode())
-        self.assertEqual(data_out["can_edit"], False)
+        self.assertEqual(data_out["can_edit"], True)
         self.assertEqual(data_out["can_join"], False)
 
     def test_get_collective_permissions_when_not_logged_in(self):
